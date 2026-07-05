7394ed9db3ff91bf129a428725704fc47d1e6f48
Boxxland <burin102016@gmail.com>
2026-07-05 20:19:12 +0700

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import VideoTemplate from './components/video/VideoTemplate';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen overflow-hidden bg-bg">
-      <VideoTemplate />
-    </div>
-  );
-}
